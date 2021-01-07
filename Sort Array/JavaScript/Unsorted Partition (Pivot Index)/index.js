62e96d110cfb1e6a388e9cce97d4ad2038d900d8
ethan-mace <ethanmace@protonmail.com>
2021-01-07 11:30:48 -0600

    down to 9 lines

--- Sort Array/JavaScript/Unsorted Partition (Pivot Index)/index.js
+++ Sort Array/JavaScript/Unsorted Partition (Pivot Index)/index.js
@@ -2,11 +2,8 @@ console.log(partition([7,9,18,6,1,23,2,10,8,4,3],3))
 function partition(arr, pivot){
     var left = []; var right = [];    
     for(i = 0; i < arr.length; i++){
-        if(arr[i] < arr[pivot] && i != pivot){
-            left.push(arr[i]);
-        } else if(i != pivot){
-            right.push(arr[i]);
-        }
+        if(arr[i] < arr[pivot] && i != pivot){left.push(arr[i]);} 
+        else if(i != pivot){right.push(arr[i]);}
     }
     return left.concat(arr[pivot], right);
 }
