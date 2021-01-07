17dcaec3519a677373ef6b35a0e654ed19265b9a
ethan-mace <ethanmace@protonmail.com>
2021-01-07 17:27:37 -0600

    fixed for duplicates

--- Sort Array/JavaScript/Unsorted Partition (Pivot Index)/index.js
+++ Sort Array/JavaScript/Unsorted Partition (Pivot Index)/index.js
@@ -1,9 +1,9 @@
-console.log(partition([7,9,18,6,1,23,2,10,8,4,3],3))
+console.log(partition([10,6,8,6,1,2,6,3],3))
 function partition(arr, pivot){
-    var left = []; var right = [];    
+    var left = []; var right = [];
     for(i = 0; i < arr.length; i++){
-        if(arr[i] < arr[pivot] && i != pivot){left.push(arr[i]);} 
-        else if(i != pivot){right.push(arr[i]);}
+        if(arr[i] <= arr[pivot] && i < pivot || arr[i] < arr[pivot] && i != pivot){left.push(arr[i]);}        
+        else if(arr[i] >= arr[pivot] && i != pivot){right.push(arr[i]);}
     }
     return left.concat(arr[pivot], right);
 }
