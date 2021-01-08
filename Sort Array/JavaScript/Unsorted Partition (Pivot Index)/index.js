30fac6e433e8ebaf0fa84ddd93d54a148634f6b3
ethan-mace <ethanmace@protonmail.com>
2021-01-08 00:12:55 -0600

    randomized array length, elements, and pivot

--- Sort Array/JavaScript/Unsorted Partition (Pivot Index)/index.js
+++ Sort Array/JavaScript/Unsorted Partition (Pivot Index)/index.js
@@ -1,9 +1,25 @@
-console.log(partition([10,6,8,6,1,2,6,3],3))
-function partition(arr, pivot){
-    var left = []; var right = [];
-    for(i = 0; i < arr.length; i++){
-        if(arr[i] <= arr[pivot] && i < pivot || arr[i] < arr[pivot] && i != pivot){left.push(arr[i]);}        
-        else if(arr[i] >= arr[pivot] && i != pivot){right.push(arr[i]);}
+// randomized array length, elements, and pivot
+console.log("Output: " + partition())
+function partition(){
+    var arrLength = Math.floor(Math.random() * 50);
+    var pivot = Math.floor(Math.random() * arrLength);
+    var arr = [];
+    var left = []; 
+    var right = [];
+    var i = 0;
+    while(i <= arrLength){
+        arr.push(Math.floor(Math.random() * 100));
+        i++;
     }
+    console.log("Generated array: " + arr);
+    for(i = 0; i < arr.length; i++){
+        if(arr[i] <= arr[pivot] && i < pivot || arr[i] < arr[pivot] && i != pivot){
+            left.push(arr[i]);
+        }        
+        else if(arr[i] >= arr[pivot] && i != pivot){
+            right.push(arr[i]);
+        }
+    }
+    console.log("Left: " + left + "\nPivot: " + arr[pivot] + " at index " + pivot + "\nRight: " + right)
     return left.concat(arr[pivot], right);
 }
